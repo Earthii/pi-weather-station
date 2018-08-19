648f4e6372182788ef15cdd6f3d7aed258b032e9
Earthii <xiao_eric@hotmail.com>
2018-08-19 16:49:51 -0400

    print

--- weatherStation.py
+++ weatherStation.py
@@ -16,35 +16,42 @@ root = tk.Tk()
 
 image = PhotoImage(file="background.gif")
 
-background=Label(root, image=image)
-background.place(x=0,y=0,relwidth=1, relheight=1)
+background = Label(root, image=image)
+background.place(x=0, y=0, relwidth=1, relheight=1)
 
 temperature = StringVar()
-temperature.set("----"+" °C")		
+temperature.set("----"+" °C")
 
 humidity = StringVar()
-humidity.set("----"+" %")		
+humidity.set("----"+" %")
 
-temperatureLabel = Label(root, fg="white", background="#00dbde", textvariable=temperature, font=("Helvetica", 40,"bold"))
+temperatureLabel = Label(root, fg="white", background="#00dbde",
+                         textvariable=temperature, font=("Helvetica", 40, "bold"))
 temperatureLabel.place(x=580, y=100)
 
-humidityLabel = Label(root, fg="white", background="#00dbde", textvariable=humidity, font=("Helvetica", 40,"bold"))
+humidityLabel = Label(root, fg="white", background="#00dbde",
+                      textvariable=humidity, font=("Helvetica", 40, "bold"))
 humidityLabel.place(x=580, y=200)
- 
-root.attributes("-fullscreen",True)
-root.bind("<1>",exit)
+
+root.attributes("-fullscreen", True)
+root.bind("<1>", exit)
+
 
 def exit():
-	root.quit()
+    root.quit()
+
 
 def readSensor():
-	root.after(2000, readSensor)
-        h,t = dht.read_retry(dht.DHT22,20)
-        temp = "%.1f" %t
-	temperature.set(temp+" °C")	
-	hum = "%.1f" %h
-	humidity.set(hum+"  %")
-		
+    root.after(2000, readSensor)
+    h, t = dht.read_retry(dht.DHT22, 20)
+    temp = "%.1f" % t
+    temperature.set(temp+" °C")
+    hum = "%.1f" % h
+    humidity.set(hum+"  %")
+    print(temp+" °C")
+        print(hum+"  %")
+
+
 root.after(2000, readSensor)
 
 root.mainloop()
